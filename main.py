7a6f9b2a9bf346cc06a63202a4e4852f709039d8
Dmytro Skrynnyk <dmytroskrynnyk@MacBook-Pro-Dmytro.local>
2022-11-23 11:28:22 +0200

    ex1_2

---
+++ main.py
@@ -0,0 +1,30 @@
+
+#numbers from 1 to 5
+#unsatisfactory = 3
+#good = 4
+#excellent = 5
+
+number = int(input("numbers from 1 to 5: "))
+if number == 4:
+    print("good")
+if number == 5:
+    print("excellent")
+if number == 3:
+    print("unsatisfactory")
+if number == 2:
+    print("unsatisfactory")
+if number == 1:
+    print("unsatisfactory")
+
+if number <= 0:
+    print("error")
+if number >= 6:
+    print("error")
+
+
+
+
+
+
+
+
